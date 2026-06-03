aa5fe7c711cb9150161d7e6c054ea742a8fb0a5b
Lucas Gomez <lucasgom3z2606@gmail.com>
2026-06-03 16:48:57 +0900

    Filter Button Added
    After lots of trouble shooting, the filter button is now added. Although It makes the page shift a lot and does not look very nice, so I will work on making some elements of the page static

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'; 
-
+//INTERFACE
 export interface MediaItem {
   id: number;
   title: string;
@@ -8,7 +8,7 @@ export interface MediaItem {
 }
 
 
-
+//TEMPORARY LIST
 const mockWatchlist: MediaItem[] = [
   {
     id: 1,
@@ -33,6 +33,7 @@ const mockWatchlist: MediaItem[] = [
 
 
 export default function App() {
+  //LIGHTMODE STARTING STATE AND SWITCH FUNCTION
   const [mode, setMode] = useState("Dark");
   function changeMode() {
     if (mode === "Dark") {
@@ -41,15 +42,39 @@ export default function App() {
       setMode("Dark");
     }
   }
+//FILTER STARTING STATE AND SWITCH FUNCTION
+  const [activeCategory, selectActiveCategory]=useState("All")
+  function changeCategory() {
+    if (activeCategory==="All")
+      selectActiveCategory("Movie");
+    else if (activeCategory === "Movie")
+      selectActiveCategory("Soundtrack");
+    else if (activeCategory === "Soundtrack")
+      selectActiveCategory("All");
+  }
+
+//HTML RETURN
   return (
     <div className={`min-h-screen flex flex-col items-center justify-center p-8 transition-colors duration-500 ${
       mode === "Light" ? "bg-slate-100 text-slate-900" : "bg-slate-900 text-white"
     }`}>
-      
       <h1 className="text-4xl font-bold mb-8">Media Watchlist!</h1>
-      
+
+
+
+      <div className="flex gap-4 mb-4">
+        <button onClick={changeCategory} 
+        className="bg-sky-600 text-white font-bold py-3 px-5 rounded-lg shadow-xl hover:bg-sky-500 active:scale-95 transition-all">
+          {activeCategory}
+        </button>
+      </div>
+
+
       <div className="flex flex-col gap-4 w-full max-w-md">
-        {mockWatchlist.map((item) => (
+        {mockWatchlist.map((item) => {
+        if (activeCategory!== "All" && item.category.toLocaleLowerCase() !== activeCategory.toLocaleLowerCase()) {
+          return null;}
+        return (
           <div 
             key={item.id} 
             className={`p-4 rounded-lg border transition-colors duration-500 shadow-md ${
@@ -65,10 +90,12 @@ export default function App() {
               {item.category} • {item.status}
             </p>
           </div>
-        ))}
+        );
+        })}
       </div>
 
-      {/* 4. The fixed button in the top left corner */}
+
+
       <button 
         onClick={changeMode} 
         className="fixed top-4 left-4 bg-sky-600 text-white font-bold py-3 px-5 rounded-lg shadow-xl hover:bg-sky-500 active:scale-95 transition-all"
